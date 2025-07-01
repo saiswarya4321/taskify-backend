f126b1a121c09d522bb26e9901eea7feac4a2312
aiswarya s <aiswaryas4906@gmail.com>
2025-07-01 18:46:51 +0530

    backend

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -5,12 +5,12 @@ const authUser = (req, res, next) => {
     
     const { token } = req.cookies;
     if (!token) {
-        return res.status(400).json({ message: "user not autenticated" })
+       return res.status(401).json({ message: "User not authenticated" });
     }
     try {
       const verifiedToken = jwt.verify(token, process.env.SECRET_KEY)
     if (!verifiedToken) {
-        return res.status(400).json({ message: "Token not verified" })
+       return res.status(403).json({ message: "Token verification failed" });
     }
     req.user = verifiedToken.data;
     next()  ;
