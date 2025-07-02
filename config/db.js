341e84b0edeadd5d8a0ebcb380bc1169a7be0ff0
aiswarya s <aiswaryas4906@gmail.com>
2025-07-02 19:12:52 +0530

    git

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose")
 
 const connectionDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://aiswaryas4906:mPx7Ysu6yew45gsp@cluster0.waa9ijc.mongodb.net/tododb");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("todo database connected successfully!")
 
     } catch (error) {
